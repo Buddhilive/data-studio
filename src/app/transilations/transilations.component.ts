616aae7a863d728b0a9e51d671e80d2daea35a9d
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-03-18 11:10:13 +0530

    Generate UI components

---
+++ src/app/transilations/transilations.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transilations',
+  imports: [],
+  templateUrl: './transilations.component.html',
+  styles: ``
+})
+export class TransilationsComponent {
+
+}
