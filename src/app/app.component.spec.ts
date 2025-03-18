e082539a5e73ed45750c7a9a30f7114283d381c8
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-03-18 09:17:32 +0530

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'datakernel' title`, () => {
+  it(`should have the 'data-studio' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('datakernel');
+    expect(app.title).toEqual('data-studio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, datakernel');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, data-studio');
   });
 });
