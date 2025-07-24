6e62376d96c0db15b91a640bb3fcb5c0f8afa7f8
Buddhi Kavindra <visitbudkavin@gmail.com>
2025-07-24 21:10:15 +0530

    Reset project

--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  supabaseUrl: 'https://qhajsfgptbdzazxaltqq.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoYWpzZmdwdGJkemF6eGFsdHFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyNzMwMTIsImV4cCI6MjA1Nzg0OTAxMn0.f3UyhgEeZjJ1x_2ZNkd7E9Xk2UXylOACRdPPATb0K_Y',
-};
